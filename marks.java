e78c08b4b0506467583b1d53146276e7f98a0c22
shirazkhan1995 <shirazkhan1995@gmail.com>
2023-10-22 21:21:10 +0530

    Java Practice

---
+++ marks.java
@@ -0,0 +1,87 @@
+import java.util.Scanner;
+public class marks{
+    public static void main(String[] args){
+       // Scanner sc=new Scanner(System.in);
+    
+        /*System.out.println("Enter marks in English");
+        float a=sc.nextInt();
+        System.out.println("Enter marks in Mathematics");
+        float b=sc.nextInt();
+        System.out.println("Enter Marks in Physics");
+        float c=sc.nextInt();
+        System.out.println("Enter marks in Chemistry");
+        float d=sc.nextInt();
+        System.out.println("Enter marks in Biology");
+        float e=sc.nextInt();
+
+        float marks=((a+b+c+d+e)/500)*100;
+        System.out.print("You got ");
+        System.out.print(marks);
+        System.out.print(" %");
+*/
+/*System.out.println("Enter any number");
+
+ boolean b1=sc.hasNextInt();
+ if(b1==true){
+    System.out.println("Yes!!,The number is integer");
+
+ }
+ else{
+    System.out.println("No!!,The number is not integer");
+ }
+ int z=34;
+ int y=55;
+ System.out.println(z<y);*/
+ /*float r=7/4.0f*9/2.0f;
+ System.out.println(r);*/
+ /*char grade='A';
+ grade=(char)(grade + 8);
+ System.out.println(grade);
+ 
+ grade=(char)(grade-8);
+ System.out.println(grade);*/
+ /*char name='A';
+ char name1='A';
+ char name2='F';
+ char name3='T';
+  char name4='A';
+   char name5='A';
+    char name6='B';
+
+name=(char)((name+8)+(name1+5)+(name2+3)+(name3+7)+(name4+9)+(name5+8)+(name6+4));
+System.out.println(name);
+*/
+/*Scanner sc=new Scanner(System.in);
+System.out.println("Specify your age");
+int age=sc.nextInt();
+switch(age){
+    case 12:System.out.println("No, you cant drive");
+    break;
+    case 60:System.out.println("Enjoy your life");
+    break;
+    case 18:System.out.println("You are an adult");
+    break;
+    default:System.out.println("This is not a valid age");
+
+
+}*/
+double x=Math.random();
+double y=(int)(x*10);
+String p="rock";
+String q="Paper";
+String r="Scissor";
+if(y>10 && y<23){
+    System.out.println(p);
+}
+else if(y<10 && y>2){
+    System.out.println(q);
+
+}
+else if(y>40 && y<87){
+    System.out.println(r);
+}
+int score=0;
+
+    
+    }
+}
---
+++ prog.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class prog{
+public static void main(String[] args){
+System.out.println("The sum of these numbers is");
+int num=2;
+int num2=5;
+int num3=6;
+int sum=num+num2+num3;
+byte age=43;
+float f1=5.6f;
+double d1=4.66;
+long gtr=5666666666l;
+char c='A';
+String q=("Gabriel");
+System.out.println(q);
+System.out.println(age);
+System.out.println(sum);
+//User Input
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter number1");
+//int a=sc.nextInt();
+boolean b1=sc.hasNextFloat();
+System.out.println(b1);
+/*System.out.println("Enter number2");
+int b = sc.nextInt();
+int add = a+b;
+System.out.println("The sum of these nmbers is:");
+System.out.println(add);*/
+
+
+
+
+}}
